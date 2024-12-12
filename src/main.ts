b2581c33786f68557077d03d288a8522f6d57a2b
agrear <malte.wassmann@hotmail.de>
2024-12-12 18:04:38 +0100

    Initial commit.

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { invoke } from '@tauri-apps/api/core';
+
+let kioskEl: HTMLIFrameElement | null;
+
+async function getUrl(): Promise<string> {
+  return await invoke('get_url');
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  kioskEl = document.querySelector('#kiosk');
+
+  if (kioskEl) {
+    kioskEl.src = await getUrl();
+  }
+});
