7fb02e9dc507c1a7511eb77f68c849791507feeb
agrear <malte.wassmann@hotmail.de>
2024-12-13 17:59:36 +0100

    Downgrade to tauri v1.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
+#[tauri::command]
+fn get_url() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    String::from(&args[1])
+}
+
 fn main() {
-    rpi_kiosk_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_url])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
