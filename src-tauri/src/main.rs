5ef44da5731123ff536576e6affa4d901368a563
agrear <malte.wassmann@hotmail.de>
2025-01-02 18:52:54 +0100

    Stop using iframe.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::env;
+use clap::Parser;
+use tauri::{Manager, PhysicalPosition, Position};
 
-#[tauri::command]
-fn get_args() -> Vec<String> {
-    env::args().collect()
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    url: String,
+
+    #[arg(long, default_value_t = false)]
+    hide_cursor: bool
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_args])
+        .setup(|app| {
+            let args = Args::parse();
+
+            let window = app.get_window("main").unwrap();
+            window.eval(&format!("window.location.replace('{0}')", args.url))?;
+
+            //window.set_cursor_visible(args.hide_cursor).unwrap();
+            // Workaround for https://github.com/tauri-apps/tauri/issues/10231
+            if args.hide_cursor {
+                let position = Position::Physical(PhysicalPosition{ x: 9999, y: 9999 });
+                window.set_cursor_position(position).unwrap();
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
